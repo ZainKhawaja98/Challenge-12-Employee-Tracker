b40a04e76be32621c81e807741e1032049f2a899
zain khawaja <zainkhawaja98@gmail.com>
2023-01-19 17:02:21 -0500

    updated package.js and seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,24 +1,51 @@
-INSERT INTO departments (department_name)
-VALUES
-        ('Human Resources'),
-        ('Engineering'),
-        ('Customer service');
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+USE employees;
+CREATE TABLE department (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL
+);
+CREATE TABLE employee (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  manager_id INT UNSIGNED  
+);
 
-
-INSERT INTO managers (manager_name)
+use employees;
+INSERT INTO department
+    (name)
 VALUES
-        ('Homer simpson'),
-        ('Bob Belcher'),
-        ('Randy Marsh');
-
-INSERT INTO roles (title, salary, department_id)
+   ('Human Resources'),
+   ('Engineering'),
+   ('Customer service'),
+   ('Marketing');
+INSERT INTO role
+    (title, salary, department_id)
 VALUES
-        ('HR Rep', 70000, 1),
-        ('Engineering executive', 120000, 2),
-        ('Customer service leader', 55000, 3);
-
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
+    ('General Manager', 400000, 1),
+    ('Assistant Manager', 300000, 1),
+    ('Sales Analyst', 600000, 2),
+    ('Customer Service Leader', 800000, 2),
+    ('Social Media Manager', 1200000, 3),
+    ('Marketing Manager', 1500000, 3),
+    ('CEO', 5000000, 4),
+    ('CFO', 3000000, 4);
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
 VALUES
-        ('John Wick', '', 1, 3),
-        ('Stephen', 'Strange', 2, 1),
-        ('Bill', 'Denbrough', 3, 2);
+    ('John', 'Wick', 1, NULL),
+    ('Stephen', 'Strange', 2, 1),
+    ('Bill', 'Denbrough', 3, NULL),
+    ('John', 'Coffey', 4, 3),
+    ('Kenny', 'McCormick', 5, NULL),
+    ('Bob', 'Belcher', 6, 5),
+    ('Homer', 'Simpson', 7, NULL),
+    ('Randy', 'Marsh', 8, 7);
